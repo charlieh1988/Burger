1b5fa3758daa13b2abeac923508a040f07d8dd29
Charlie Hilton <charllie.hiltons@gmail.com>
2019-01-29 20:51:09 -0600

    files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured)
+
+VALUES ('McDonalds Big Mac', 'True');
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('McDonalds Quarter Pounder', 'False');
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('P. Terry Vegan Burger', 'True');
+
